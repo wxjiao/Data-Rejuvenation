6e3868b15c9caf467a7b77d4d9a2a06fcbe6633f
Jiao, Wenxiang <31032829+wxjiao@users.noreply.github.com>
2020-05-29 15:30:28 +0800

    Update sh_preprocess_extra.sh

--- fairseq/commands/en_fr/sh_preprocess_extra.sh
+++ fairseq/commands/en_fr/sh_preprocess_extra.sh
@@ -1,12 +1,12 @@
-DATA=wmt14_en_fr_base_untied_probdiff01rm
+DATA=wmt14_en_fr_base_active
 TEXT=dataset/$DATA
 # Preprocess
 cd ../../
 python preprocess.py \
   --source-lang en \
   --target-lang fr \
-  --validpref $TEXT/valid_probRepLev10 \
-  --destdir data-bin/$DATA \
+  --validpref $TEXT/inactive \
+  --destdir data-bin/$DATA/inactive \
   --workers 32 \
   --srcdict data-bin/$DATA/dict.en.txt \
   --tgtdict data-bin/$DATA/dict.fr.txt \
