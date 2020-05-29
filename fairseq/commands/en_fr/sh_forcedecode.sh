4def11dc2e26b782fe8d322ffd736df677d79ea1
Jiao, Wenxiang <31032829+wxjiao@users.noreply.github.com>
2020-05-29 15:24:08 +0800

    Update sh_forcedecode.sh

--- fairseq/commands/en_fr/sh_forcedecode.sh
+++ fairseq/commands/en_fr/sh_forcedecode.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-DATA=wmt14_en_fr_base_untied_fp16
+DATA=wmt14_en_fr_base
 cd ../../
-DISK=/apdcephfs/share_916081/joelwxjiao/checkpoints
+DISK=./checkpoints
 CHECKPOINT_DIR=$DISK/$DATA
 EVAL_OUTPUT_PATH=./results/$DATA/evaluation/
 CHECKPOINT=checkpoint_best.pt
@@ -44,7 +44,4 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python force_decode.py data-bin/$DATA \
   --valid-decoding-path $EVAL_OUTPUT_PATH \
   --multi-bleu-path ./scripts/ \
  # |& tee ./results/$DATA/logs/train.log
- #--share-decoder-input-output-embed \ 
- # --save-interval 1 \
- # --keep-interval-updates 5 \
- # --keep-last-epochs 5 \
+
