8f4ef6c6c217ec6f59be0282de0884eb76d4e94a
Jiao, Wenxiang <31032829+wxjiao@users.noreply.github.com>
2020-05-29 15:52:24 +0800

    Update sh_preprocess_extra.sh

--- fairseq/commands_lstm/en_de/sh_preprocess_extra.sh
+++ fairseq/commands_lstm/en_de/sh_preprocess_extra.sh
@@ -1,12 +1,12 @@
-DATA=wmt14_en_de_transfer_TinyBig
+DATA=wmt14_en_de_lstm_active
 TEXT=dataset/$DATA
 # Preprocess
 cd ../../
 python preprocess.py \
   --source-lang en \
   --target-lang de \
-  --validpref $TEXT/valid_probRepLev10 \
-  --destdir data-bin/$DATA \
+  --validpref $TEXT/inactive \
+  --destdir data-bin/$DATA/inactive \
   --workers 32 \
   --srcdict data-bin/$DATA/dict.en.txt \
   --tgtdict data-bin/$DATA/dict.de.txt \
