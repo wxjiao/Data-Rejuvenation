82c99581dc46ac1dd5d20a493d04fc07471552ec
Jiao, Wenxiang <31032829+wxjiao@users.noreply.github.com>
2020-05-29 15:43:48 +0800

    Update sh_preprocess.sh

--- fairseq/commands_conv/en_fr/sh_preprocess.sh
+++ fairseq/commands_conv/en_fr/sh_preprocess.sh
@@ -1,4 +1,4 @@
-DATA=wmt14_en_fr_transfer_LightLightLight
+DATA=wmt14_en_fr_lightconv
 TEXT=dataset/$DATA
 # Preprocess
 cd ../../
@@ -10,6 +10,7 @@ python preprocess.py \
   --testpref $TEXT/test \
   --destdir data-bin/$DATA \
   --workers 32 \
-  --srcdict data-bin/$DATA/dict.en.txt \
-  --tgtdict data-bin/$DATA/dict.fr.txt \
+  --joined-dictionary \
+#  --srcdict data-bin/$DATA/dict.en.txt \
+#  --tgtdict data-bin/$DATA/dict.fr.txt \
 
