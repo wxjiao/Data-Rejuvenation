327423194b15e742615f933080015d2c38b4ad42
Jiao, Wenxiang <31032829+wxjiao@users.noreply.github.com>
2020-05-29 15:51:48 +0800

    Update sh_preprocess.sh

--- fairseq/commands_lstm/en_de/sh_preprocess.sh
+++ fairseq/commands_lstm/en_de/sh_preprocess.sh
@@ -1,4 +1,4 @@
-DATA=wmt14_en_de_transfer_BaseLSTM
+DATA=wmt14_en_de_lstm
 TEXT=dataset/$DATA
 # Preprocess
 cd ../../
@@ -10,6 +10,6 @@ python preprocess.py \
   --testpref $TEXT/test \
   --destdir data-bin/$DATA \
   --workers 32 \
-  --srcdict data-bin/$DATA/dict.en.txt \
-  --tgtdict data-bin/$DATA/dict.de.txt \
+#  --srcdict data-bin/$DATA/dict.en.txt \
+#  --tgtdict data-bin/$DATA/dict.de.txt \
 
