60994ac239a3579f9294444faa362d74f38ef503
Jiao, Wenxiang <31032829+wxjiao@users.noreply.github.com>
2020-05-29 15:32:41 +0800

    Update sh_generate.sh

--- fairseq/commands_conv/en_de/sh_generate.sh
+++ fairseq/commands_conv/en_de/sh_generate.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 cd ../../
-DATASET=wmt14_en_de_transfer_BaseLightLight
+DATASET=wmt14_en_de_lightconv
 DATA=data-bin/$DATASET
-CP_PATH=/apdcephfs/share_916081/joelwxjiao/checkpoints/$DATASET
+CP_PATH=./checkpoints/$DATASET
 CP=checkpoint_best.pt
 
 CHECKPOINT=$CP_PATH/$CP
@@ -12,7 +12,7 @@ mkdir $VALID_DECODE_PATH
 
 SUBSET=valid
 echo "Evaluate on $DATA with $CHECKPOINT"
-CUDA_VISIBLE_DEVICES=7 python generate.py \
+CUDA_VISIBLE_DEVICES=0 python generate.py \
   data-bin/$DATASET \
   -s en \
   -t de \
